e3a86f561f7aa15bac0566b4f2a93f505e61fe78
lidefi87 <lidefi87@hotmail.com>
2025-04-30 15:52:56 +1000

    using xp65

--- scripts/dbpm_job.sh
+++ scripts/dbpm_job.sh
@@ -2,15 +2,14 @@
 
 #PBS -P vf71
 #PBS -q normalbw
-#PBS -l ncpus=84
-#PBS -l mem=756GB
-#PBS -l jobfs=200GB
-#PBS -l walltime=04:00:00
-#PBS -l storage=gdata/vf71+gdata/hh5
+#PBS -l ncpus=14
+#PBS -l mem=126GB
+#PBS -l walltime=12:00:00
+#PBS -l storage=gdata/vf71+gdata/xp65
 #PBS -M lilian.fierroarcos@utas.edu.au
 #PBS -m abe
 #PBS -l wd
 
-module use /g/data/hh5/public/modules
-module load conda/analysis3
+module use /g/data/xp65/public/modules
+module load conda/analysis3-25.04
 python3 run_dbpm_gridded.py
